73658a0f89b55ced1044299599ef853ed60b7b25
Eslam <e.yasser997@gmail.com>
2023-04-05 02:16:02 +0200

    Program to find the minimum & maximum value of array elements.

--- lab 3/Lab_3.01/main.c
+++ lab 3/Lab_3.01/main.c
@@ -14,22 +14,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define INIT 0
-#define Lenth 5
+#define Length 5
 int main()
 {
     printf("Hello enter the\n");
     //array of zeros
-    int arr[5] = {INIT};
+    int arr[Length] = {INIT};
 
     //Due to arrays are once created once Never Changed so override on element every round by scanf();
-    for(int i=0; i < Lenth;i++){
+    for(int i=0; i < Length;i++){
         printf("Enter the number to be the %d element\n",i+1);
         scanf("%d",&arr[i]);
     }
 
     //this for print array elements one by one
-    int j = INIT;
-    while(j < Lenth){
+    int j = 0;
+    while(j < Length){
         printf("\nElement Number %d is : %d \n",j+1,arr[j]);
         j++;
     };

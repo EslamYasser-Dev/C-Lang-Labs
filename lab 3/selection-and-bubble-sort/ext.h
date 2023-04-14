bb4b4b0d7c4b9938193dfa85b782770275ea801b
eslamyasser <e.yasser997@gmail.com>
2023-04-14 22:39:21 +0200

    C-Lang-Labs

---
+++ lab 3/selection-and-bubble-sort/ext.h
@@ -0,0 +1,9 @@
+#ifndef ext
+#define ext
+void swapp (int* first, int* second);
+void printArrays (int shwArr[], int n);
+void bubbleSorting(int arr[], int n);
+void seclectionSorting(int arr[], int n);
+void insertionSorting(int arr[], int n);
+
+#endif
